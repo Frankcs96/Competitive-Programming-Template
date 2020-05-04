8fed1bbebab108769500a5525de96809e09866b8
Francisco Carmona Suárez <fcsuarez96@gmail.com>
2020-05-04 05:05:24 +0200

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	// You can start writing your solutions here
+
+
+
+}
